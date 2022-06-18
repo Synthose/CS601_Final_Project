49cbe6d032084e4bc66bc3fb39912bec9d39d869
Alan Krajbich <synthose@bu.edu>
2022-06-18 15:51:13 -0700

    updated Vue

--- CS601_TermProject_Alan_Krajbich/scripts/onLoad.js
+++ CS601_TermProject_Alan_Krajbich/scripts/onLoad.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded",function () {
     const navBar = document.querySelectorAll("nav");
     navBar.forEach(element => {
-        element.innerHTML = "<a href=\"home.html\">Main</a> | <a href=\"blog.html\">Blog</a> | <a href=\"faqs.html\">FAQs</a> | <a href=\"contact.html\">Contact Me</a> "
+        element.innerHTML = "<a href=\"home.html\">Main</a> | <a href=\"blog.html\">Blog</a> | <a href=\"faqs.html\">FAQs</a> | <a href=\"aboutMe.html\">About me </a> |<a href=\"contact.html\">Contact Me</a> "
     });
 })
