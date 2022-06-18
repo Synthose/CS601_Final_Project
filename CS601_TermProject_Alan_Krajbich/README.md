d60cbfecb23ac398454b62dc05ca3ad3e80955db
Alan K <synthose@gmail.com>
2022-06-18 14:40:41 -0700

    Add files via upload

---
+++ CS601_TermProject_Alan_Krajbich/README.md
@@ -0,0 +1,7 @@
+#README for Assignment 1
+To run this project, simply open main.html in a modern, updated browser. make sure the folder structure is unchanged from the zip file. 
+
+##Author Info
+Alan Krajbich
+Synthose@bu.edu
+Synthose@gmail.com
---
+++ CS601_TermProject_Alan_Krajbich/scripts/aboutMe.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+
+<script>
+  export default {
+    name: "#app",
+      data() {
+        return{
+          courseName: "S601: Web Application Development",
+          link: "http://www.bu.edu/csmet/academic-programs/courses/cs601/"
+        }
+      }
+  }
+</script>
+
+<style>
+</style>
