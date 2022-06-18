aa041ac3438fa368bb107c58626ad03cef279323
Alan Krajbich <synthose@bu.edu>
2022-06-18 16:10:56 -0700

    swapped Vue to JS

---
+++ CS601_TermProject_Alan_Krajbich/scripts/aboutMe.js
@@ -0,0 +1,9 @@
+export default {
+  name: "#app",
+    data() {
+      return{
+        courseName: "S601: Web Application Development",
+        link: "http://www.bu.edu/csmet/academic-programs/courses/cs601/"
+      }
+    }
+}
