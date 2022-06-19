d87dd4ecc9ef731196a3349021d905f1de899ebe
Alan Krajbich <synthose@bu.edu>
2022-06-19 00:21:03 -0700

    finally it saves

--- CS601_TermProject_Alan_Krajbich/scripts/steamAPI.js
+++ CS601_TermProject_Alan_Krajbich/scripts/steamAPI.js
@@ -1,30 +1,28 @@
 const url = 'https://www.dnd5eapi.co/api/'; 
-let classes;
-let races;
-let alignment;
+let classes = {};
+let races= {};
+let alignment= {};
 let scores;
 
 document.addEventListener("DOMContentLoaded",function () {
-    classes = getCategory("/classes/", classes);
-    races = getCategory("/races/",races);
-    alignment = getCategory("/alignments/",alignment);    
+    getCategory("/classes/", classes);
+    getCategory("/races/",races);
+    getCategory("/alignments/",alignment);    
+    console.log(classes);
 })
-async function getCategory(suffix, obj){
-  // let request = new XMLHttpRequest();
-  // request.open("GET", url+suffix, true);
-  // request.send();  
+function getCategory(suffix, obj){
+  let request = new XMLHttpRequest();
+  request.open("GET", url+suffix, true);
+  request.send();  
 
-  // request.onreadystatechange = function() {
-  //     if (this.readyState === 4 && this.status === 200) {
-  //       const response = JSON.parse(this.responseText);
-  //       console.log(response.results);
-  //       return response.results;
-  //     }
-  //   };
-  // fetch(url+suffix).then(response => response.json()).then(response => obj = response.results);
-  const response = await fetch(url+suffix);
-  return response.json().results;
-
+  request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        console.log(response.results);
+        obj.values = response.results;
+        return response.results;
+      }
+    };
 }
 function clicked(){
     event.preventDefault();
