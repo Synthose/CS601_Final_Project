7d956c06d608a0303f5b3a4c6190dac47ee2f7e2
Alan Krajbich <synthose@bu.edu>
2022-06-18 18:37:13 -0700

    Adding 2 Truths and Lie

--- CS601_TermProject_Alan_Krajbich/scripts/aboutMe.js
+++ CS601_TermProject_Alan_Krajbich/scripts/aboutMe.js
@@ -5,15 +5,31 @@ export default {
         snappyMessage: 'awating message',
         link: "http://www.bu.edu/csmet/academic-programs/courses/cs601/",
         messages: [
-          "hello",
-           "test",
-           "Happy Happy Day"
-        ]
+          "Thank you for the great semester",
+           "These are random!",
+           "Refresh Again to see more!",
+           "I am a CS student at BU",
+           "Power Overwhelming",
+           "All your base are belong to us"
+        ],
+        correct: "You guessed correctly! Congratulations!",
+        incorrect: "You guessed incorrectly! Try again!",
+        correctColor: "green",
+        incorrectColor: "red",
+        unknownColor: "grey",
+        question_1_response: ""
       }
     },
     methods: {
       setRandomMessage: function() {
         this.snappyMessage = this.messages[Math.floor(Math.random() * this.messages.length)];
+      },
+      checkIfLie(itemClass){
+        if(itemClass === "true"){
+          this.question_1_response = this.correct;
+        }else{
+          this.question_1_response = this.incorrect;
+        }
       }
     },
     computer: {
